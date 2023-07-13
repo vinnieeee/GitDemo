ee8574aade4deffe252c7f7c1023855a96d58ee2
vinnieeee <92759192+vinnieeee@users.noreply.github.com>
2023-07-13 12:15:17 +0530

    entire project

---
+++ src/main/java/DemoProj/DemoProj/App.java
@@ -0,0 +1,13 @@
+package DemoProj.DemoProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/DemoProj/DemoProj/BasicProject.java
@@ -0,0 +1,19 @@
+package DemoProj.DemoProj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasicProject {
+	
+	public static void main(String[] args)
+	{
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+
+	}
+
+}
